f1833d31ae862dbabce67d7df6d612fc26dc081b
Shadrack S <shadrackshibu1@yahoo.com>
2024-11-30 13:45:11 +0530

    Render Data & Spliting Code * Render sample mock data and itrated in the code *Started Frontend Code Spliting for reusablity

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const product = [
+  {
+    title:"Samsung S23 Ultra 5G",
+    description:"Made By Samsung ",
+    productImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS29QFQZeYjtzL10rPxlp2g4VZf8WWDIBtKBA&s",
+    price:"90,999",
+    currency:"INR",
+    rating:"4.5"
+
+  },
+  {
+    title:"Samsung S24 Ultra 5G",
+    description:"Made By Samsung ! ",
+    productImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS29QFQZeYjtzL10rPxlp2g4VZf8WWDIBtKBA&s",
+    price:"1,24,999",
+    currency:"INR",
+    rating:"4.4"
+
+  },{
+    title:"Iphone 15 Pro Max 512 GB",
+    description:"Made By Apple ",
+    productImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFWYKGh7XS7mLvN-979p7Sxx3qBZvCGOqMzw&s",
+    price:"99,999",
+    currency:"INR",
+    rating:"4.4"
+
+  }
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { product});
 });
 
 module.exports = router;
